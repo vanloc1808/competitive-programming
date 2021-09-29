cccfefec9d8de3865e0d9bd25b6118d3df6e24b2
vanloc1808 <vanloc1808@users.noreply.github.com>
2021-09-29 21:19:49 +0700

    change to problem C, Linear Algorithm 2, change the return type of solve to long long, Sep 29 2021

--- CP_Practices/[Coursera]CP for Beginners/LinearAlgorithm2_ExerciseC.cpp
+++ CP_Practices/[Coursera]CP for Beginners/LinearAlgorithm2_ExerciseC.cpp
@@ -32,49 +32,49 @@ const double MOD = 1e9 + 7;
 const double PI = 3.14159;
 const double EPSILON = 1e-6;
 
-int solve() {
-    // int n;
-    // cin >> n;
-
-    
-    // vll a(n);
-    // vll maxValue(n);
-
-
-
-    // for (int i = 0; i < n; i++) {
-    //     cin >> a[i];
-    // }
-
-    // maxValue[0] = a[0];
-
-    // for (int i = 1; i < n; i++) {
-    //     maxValue[i] = max(a[i], maxValue[i - 1] + a[i]);
-    // }
-
-    // ll ans = maxValue[0];
-
-    // for (int i = 1; i < n; i++) {
-    //     ans = max(ans, maxValue[i]);
-    // }
-
-    // return ans;
-    /*source: Nguyen Van Hung*/
-    int n; 
+long long solve() {
+    int n;
     cin >> n;
 
-	long ans = INT_MIN;
-
-	long sum = 0, mn = 0;
-
-	for (int i = 0; i < n; i++){
-		int x; cin >> x;
-		sum += x;
-		ans = max(ans, sum - mn);
-		mn = min(mn, sum);
-	}
     
-	return ans;
+    vll a(n);
+    vll maxValue(n);
+
+
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    maxValue[0] = a[0];
+
+    for (int i = 1; i < n; i++) {
+        maxValue[i] = max(a[i], maxValue[i - 1] + a[i]);
+    }
+
+    ll ans = maxValue[0];
+
+    for (int i = 1; i < n; i++) {
+        ans = max(ans, maxValue[i]);
+    }
+
+    return ans;
+    /*source: Nguyen Van Hung*/
+    // int n; 
+    // cin >> n;
+
+	// long ans = INT_MIN;
+
+	// long sum = 0, mn = 0;
+
+	// for (int i = 0; i < n; i++){
+	// 	int x; cin >> x;
+	// 	sum += x;
+	// 	ans = max(ans, sum - mn);
+	// 	mn = min(mn, sum);
+	// }
+    
+	// return ans;
 }
 
 int main()
